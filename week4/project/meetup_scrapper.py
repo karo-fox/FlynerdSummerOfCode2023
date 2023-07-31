c5f02a953bae85ab86e7a3322d5994320172b1d3
karo-fox <karolina.kozubik@gmail.com>
2023-07-31 12:45:53 +0200

    week 4 project

---
+++ week4/project/meetup_scrapper.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://www.meetup.com/pl-PL/find/?source=EVENTS&location=pl--Gliwice&categoryId=546'
+
+result = requests.get(url)
+doc = BeautifulSoup(result.text, "html.parser")
+
+events = []
+for elem in doc.find_all('div', 'p-0 bg-clip-padding bg-cover bg-transparent relative h-full flex bg-white z-0 break-words transition-shadow duration-300 w-full flex-row justify-start py-4 border-t border-gray3 md:pt-4 md:pb-5'):
+    nested = elem.find_all('div', 'overflow-hidden w-full')[0]
+    date = ' '.join(map(lambda x: x.contents[0], nested.find('time').find_all('span')[:1]))
+    subject = nested.find('h2').contents[0]
+    organizer = nested.find_all('p', 'line-clamp-1 md:hidden')[0].contents[1]
+    location = nested.find_all('p', 'line-clamp-1 md:hidden')[1].contents[0]
+    event = {
+        'date': date,
+        'location': location,
+        'subject': subject,
+        'organizer': organizer,
+    }
+    events.append(event)
+
+for event in events:
+    print(f'{event["subject"]} by {event["organizer"]}')
+    print(f'{event["date"]} in {event["location"]}')
+    print()
+
